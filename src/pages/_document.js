f3018e99d37deb3e9ee5b141927ac5e4c7783a19
Jeancarlo Morales <jcode2006@gmail.com>
2023-03-17 16:40:12 -0500

    Add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link
+          rel='icon'
+          href='/logo.png'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
